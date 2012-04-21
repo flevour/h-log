2be4aebb5c93887634338feae7bdb2e07376a01e
Francesco Levorato <git@flevour.net>
2012-04-21 11:55:04 +0200

    sender script

---
+++ sender.php
@@ -0,0 +1,14 @@
+<?php
+$urlLogger = "http://hlog.local/log.php?facility=%s&machine_id=%s&line=%s";
+
+$f = fopen('php://stdin', 'r');
+while ($logLine = fgets($f))
+	$facility = $_SERVER['argv'][1];
+	$machine_id = $_SERVER['SERVER_ADDR'];
+	$line = urlencode($logLine);
+	$ch = curl_init(sprintf($urlLogger, $facility, $machine_id, $line);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_exec($ch);
+	curl_close($ch);
